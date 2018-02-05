0b12ef1f255d97c78fb5afdad4a7e1bb645c7bfd
Philippe Dionne <dionne.phil@gmail.com>
2018-02-05 08:47:48 -0500

    Update client spec

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -1,42 +1,23 @@
 require 'spec_helper'
 
 describe Flinks::Client do
-  after do
-    Flinks.reset!
-  end
 
-  context "with module configuration" do
-    before do
-      Flinks.configure do |config|
-        config.customer_id = 'token'
-      end
+  describe "#initialize" do
+    it "without required arguments" do
+      expect { Flinks::Client.new }.to raise_error KeyError
     end
 
-    it "inherits the module configuration" do
-      expect(Flinks.send(:customer_id)).to eq('token')
-    end
-  end
-
-  context "with class configuration" do
-    before do
-      Flinks.configure do |config|
-        config.customer_id = 'token'
-      end
+    it "with required arguments" do
+      client = Flinks::Client.new(customer_id: 'token')
+      expect(client.customer_id).to eq('token')
     end
 
-    let(:client) { Flinks.new(customer_id: 'another') }
-
-    it "overrides the module configuration after initialization" do
-      expect(client.customer_id).to eq('another')
-    end
-  end
-
-  describe "#request" do
-    let(:client) { Flinks.new(customer_id: 'token') }
-
-    it "raises on absent customer_id" do
-      client.customer_id = nil
-      expect { client.get("whatever") }.to raise_error(ArgumentError)
+    it "sets defaults" do
+      client = Flinks::Client.new(customer_id: 'token')
+      expect(client.api_endpoint).not_to be_nil
+      expect(client.user_agent).not_to be_nil
+      expect(client.on_error).not_to be_nil
+      expect(client.debug).not_to be_nil
     end
   end
 end
