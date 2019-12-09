2e8844e85a9e4dee132d25664a77f283962e9f98
Philippe Dionne <dionne.phil@gmail.com>
2019-12-09 13:36:35 -0500

    Remove non-standard response handling

--- lib/flinks/api/card.rb
+++ lib/flinks/api/card.rb
@@ -8,9 +8,7 @@ module Flinks
       # @param [String] login_id
       # @return [Hash]
       def delete_card(login_id:)
-        delete("#{customer_id}/BankingServices/DeleteCard/#{login_id}").tap do |response|
-          raise Flinks::NotFound, "Invalid LoginId" if response.match(/Invalid LoginId/)
-        end
+        delete("#{customer_id}/BankingServices/DeleteCard/#{login_id}")
       end
     end
   end
